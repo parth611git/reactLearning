dab4c105477dd7b9c5133bdd46b6a354ba706cd8
Parth Patel <parth.orbit@gmail.com>
2017-06-20 13:47:15 +0530

    Correct code by adding props to Form component

--- Card.jsx
+++ Card.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import ReactDOM from 'react-dom';
 
 class Card extends React.Component
 {
@@ -32,7 +32,9 @@ class Card extends React.Component
 }
 
 class CardList extends React.Component
-{
+{    
+   /* Rendering GIT user info using multiple cards. */ 
+   /*
    render()
    {
        return(
@@ -42,6 +44,82 @@ class CardList extends React.Component
                </div>
        );
    }
+   */
 
+  /* Rendering GIT users with using login name array */
+  /*constructor(props)
+   {
+       super(props);
+       this.state = {
+        logins : ['parth611git','ketulpatel215']};
+  }
+  render()
+  {
+      var loginNameArr = this.state.logins.map(function(login)
+      {
+          return <Card name={login} />;
+      });
+
+      return (
+          <div>
+              {loginNameArr}
+              </div>);
+
+  }*/
+
+  /* Render Git user info using Form */
+  constructor(props)
+   {
+       super(props);
+       this.state = {
+        logins : []};
+        this.addCard = this.addCard.bind(this);
+  }
+  addCard(logintoAdd)
+  {
+    this.setState({logins: this.state.logins.concat(logintoAdd)});
+  };
+  render()
+  {
+      var loginNameArr = this.state.logins.map(function(login)
+      {
+          return <Card name={login} />;
+      });
+
+      return (
+          <div>
+              <Form addCard={this.addCard} />
+              {loginNameArr}
+              </div>);
+
+  }
+}
+
+class Form extends React.Component
+{
+    
+  constructor(props)
+   {
+       super(props);
+       this.handleSubmit = this.handleSubmit.bind(this);
+   }
+    handleSubmit(e)
+    {
+        e.preventDefault();
+        
+        var inputLoginName = ReactDOM.findDOMNode(this.refs.login);
+console.log(inputLoginName.value);
+        this.props.addCard(inputLoginName.value);
+        inputLoginName = "";
+    }
+    render()
+    {
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <input placeholder="git username" ref="login"/>
+                <button>Add</button>
+                </form>
+        );
+    }
 }
 export default CardList;
